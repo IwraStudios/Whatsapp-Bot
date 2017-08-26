050561b0293d1b7e1632968b0539c115607878ef
IwraStudios <IwraStudios@users.noreply.github.com>
2017-08-27 00:02:57 +0200

    Added some details on the example program

--- FirefoxExample/Program.cs
+++ FirefoxExample/Program.cs
@@ -20,11 +20,14 @@ namespace FirefoxExample
             driver = new FirefoxWApp();
             driver.StartDriver();
 
+            //Wait till we are on the login page
             while(!driver.OnLoginPage())
             {
                 Thread.Sleep(1000);
                 Console.WriteLine("Not on login page");
             }
+            
+            //We are on the login page so wait for login(also save QR to file)
             driver.GetQRImage().Save("QR.jpg", System.Drawing.Imaging.ImageFormat.Jpeg); //Get QR code and save to file
             int counter = 0;
             while (driver.OnLoginPage())
@@ -41,11 +44,15 @@ namespace FirefoxExample
 
             Console.WriteLine("You have logged in");
             
+            //IMPORTANT: Setup for the auto-replier(this.OnMsgRec)
             driver.OnMsgRecieved += OnMsgRec;
             Task.Run(driver.MessageScanner);
+            //IMPORTANT
+            
             Console.WriteLine("Use CTRL+C to exit");
             while (true)
             {
+                //Check if phone is connected, because why not
                 if (!driver.IsPhoneConnected())
                 {
                     Console.WriteLine("Phone is not connected");
@@ -54,15 +61,20 @@ namespace FirefoxExample
             }
         }
 
+        //Function which will recieve all messages
         static void OnMsgRec(WebWhatsappAPI.BaseClass.MsgArgs arg)
-        {
+        {            
+            //show message with timestamp in console
             Console.WriteLine(arg.Sender + " Wrote: " + arg.Msg + " at " + arg.TimeStamp.ToString());
 
             JavaScriptSerializer ser = new JavaScriptSerializer();
             using (WebClient wc = new WebClient())
             {
+                //Get random qoute from someone
                 var json = wc.DownloadString("https://random-quote-generator.herokuapp.com/api/quotes/random");
                 dynamic usr = ser.DeserializeObject(json);
+                
+                //Send message to the origional Sender
                 driver.SendMessage(usr["quote"] + "\n -" + usr["author"], arg.Sender);
                 return;
             }
